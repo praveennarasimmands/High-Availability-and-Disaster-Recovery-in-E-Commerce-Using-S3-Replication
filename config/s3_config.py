89e04f74c949ff5d36c8a6c6ca0fac55ce83d39e
Praveen Narasimman <hellopraveennarasimmanyt@gmail.com>
2025-01-16 12:34:26 +0530

    Create s3_config.py

---
+++ config/s3_config.py
@@ -0,0 +1,7 @@
+config = {
+    'source_bucket': 'your_source_bucket_name',
+    'destination_bucket': 'your_destination_bucket',
+    'source_region': 'source_region_code',
+    'destination_region': 'target_region_code',
+    'iam_role_arn': 'provide_arn'
+}
